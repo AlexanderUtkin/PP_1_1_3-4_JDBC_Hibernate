d200eeee1c272e5c0d5bde066fe08055c6b43428
Alexander <alexander.utkin17@gmail.com>
2022-05-31 23:29:23 +0300

    1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private final String URL = "jdbc:mysql://localhost:3306/mysql";
+    private final String ROOT = "root";
+    private final String PASSWORD = "FyAX&a%1P3l9";
+
+    Connection connection;
+
+    Driver driver;
+
+    {
+        try {
+            driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL,ROOT,PASSWORD);
+            if (!connection.isClosed()){
+                System.out.println("Соединен с БД");
+            }
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
