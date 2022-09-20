d8ac4cb21adf095d5513aa676974e900580a4cce
Alexander <alexander.utkin17@gmail.com>
2022-09-21 01:06:41 +0300

    JDBCcommit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,4 +1,5 @@
 package jm.task.core.jdbc.dao;
+
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import java.sql.*;
@@ -6,77 +7,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class UserDaoJDBCImpl implements UserDao {
 
-        Connection connection = Util.getConnection();
+    Connection connection = Util.getConnection();
 
     public UserDaoJDBCImpl() {
 
     }
 
     public void createUsersTable() {
-
         String sqlCom = "CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(20), lastName VARCHAR(20), age TINYINT)";
 
-        try {
-            Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sqlCom);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-
-
-    }
-
-    public void dropUsersTable() {
-
-        String sqlCom = "DROP TABLE IF EXISTS users";
-
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(sqlCom);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-
-    }
-
-    public void saveUser(String name, String lastName, byte age) {
-
-        String sqlCom = "INSERT INTO users(name, lastname, age) VALUES(?, ?, ?)";
-
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sqlCom);
-            preparedStatement.setString(1,name);
-            preparedStatement.setString(2,lastName);
-            preparedStatement.setByte(3,age);
-            preparedStatement.executeUpdate();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public void dropUsersTable() {
+        String sqlCom = "DROP TABLE IF EXISTS users";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sqlCom);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        String sqlCom = "INSERT INTO users(name, lastname, age) VALUES(?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlCom)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
         String sqlCom = "DELETE FROM users WHERE id";
-        try {
-            Statement statement = connection.createStatement();
+
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sqlCom);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 
     public List<User> getAllUsers() {
         List<User> allUsers = new ArrayList<>();
 
-        try {
-            Statement statement = connection.createStatement();
+        try (Statement statement = connection.createStatement()) {
             ResultSet rs = statement.executeQuery("SELECT id, name, lastName, age FROM users");
-
             while (rs.next()) {
                 User user = new User();
                 user.setId(rs.getLong(1));
@@ -94,12 +80,11 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void cleanUsersTable() {
         String sqlCom = "TRUNCATE users";
-        try {
-            Statement statement = connection.createStatement();
+
+        try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sqlCom);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,23 +7,19 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/mysql";
     private static final String ROOT = "root";
     private static final String PASSWORD = "root";
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(URL, ROOT, PASSWORD);
             System.out.println("Соединен с БД");
-        }catch (SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Ошибка соединения");
         }
         return connection;
     }
-
-
 }
